ad92aa957e4b55bbf81387df57a0bf86c033bb86
Carlos Gutierrez <feucarlos@gmail.com>
2020-07-13 23:24:28 -0500

    función Toast agregada a BitacoraService

--- src/app/services/bitacora.service.ts
+++ src/app/services/bitacora.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FaltaItem } from '../models/falta-item.model';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,8 @@ import { FaltaItem } from '../models/falta-item.model';
 export class BitacoraService {
 
   faltas: FaltaItem[] = [];
-
-  constructor() {
+  
+  constructor(public toastController: ToastController) {
     this.loadStorage();
   }
 
@@ -28,13 +29,28 @@ export class BitacoraService {
   agregarFalta(fecha: string, desc?: string) {
     const fhl = new Date( fecha );
     fhl.setTime( fhl.getTime() + fhl.getTimezoneOffset() * 60 * 1000 );
+    // Buscar si no existe una falta con la misma fecha
     const falta = new FaltaItem(fhl, desc);
-    this.faltas.push(falta);
-    this.saveStorage();
+    if(this.faltas.filter( fd => fd.fecha === falta.fecha )){
+      this.presentToast('Ya existe un registro con esa fecha');
+    } else
+    {
+      this.faltas.push(falta);
+      this.saveStorage();
+    }
   }
 
   borrarFalta(falta: FaltaItem){
     this.faltas =  this.faltas.filter( listaData => listaData.id !== falta.id);
     this.saveStorage();
   }
+
+  async presentToast( msg: string ) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+
 }
