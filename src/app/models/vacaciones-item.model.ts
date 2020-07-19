87a363e82c0fc5b0877c5a6d873b64cc64d7a99f
Carlos Gutierrez <feucarlos@gmail.com>
2020-07-18 19:03:59 -0500

    modelo de vacaciones actualizado

--- src/app/models/vacaciones-item.model.ts
+++ src/app/models/vacaciones-item.model.ts
@@ -2,12 +2,14 @@ export class VacacionesItem{
     id: number;
     desc: string;
     fecha: Date;
+    fechaDias: number;
     ymd: number;
 
-    constructor(fecha: Date, desc?: string ) {
+    constructor(fecha: Date, fechaDias: number, desc?: string ) {
         this.id = new Date().getTime();
         if ( desc ) { this.desc = desc; };
         this.fecha = fecha;
+        this.fechaDias = fechaDias;
         this.ymd = this.fecha.getFullYear() * 10000 + this.fecha.getMonth() * 100 + this.fecha.getDate();
     }
 }
